a15555929111bd067576d7551f1b3eff142e4878
ShyamalMana <shyamalmana80@gmail.com>
2024-04-04 23:11:13 +0530

    formated

--- FIBONACCI_SERIES/src/COM/SHYAM/Fibonacci.java
+++ FIBONACCI_SERIES/src/COM/SHYAM/Fibonacci.java
@@ -2,32 +2,27 @@ package COM.SHYAM;
 
 import java.util.Scanner;
 
-public class Fibonacci
-{
-	
-	void ad(int n)
-	 {
-		
-	  int p=0;
-	  int q=1;
-	  System.out.println(p+" "+q);
-	  for(int i=1;i<=n;i++)
-	  {
-	   int r=p+q;
-	   System.out.println(r);
-	   p=q;
-	   q=r;
-	  }
-	 }
-	
-	
-	 public static void main(String[]args)
-	 {
-	  Fibonacci f = new Fibonacci();
-	  Scanner sc=new Scanner(System.in);
-	  System.out.println("Enter a number to check--");
-		int n=sc.nextInt();
-	  f.ad(n);
-	 }
+public class Fibonacci {
 
-	 }
+	void ad(int n) {
+
+		int p = 0;
+		int q = 1;
+		System.out.println(p + " " + q);
+		for (int i = 1; i <= n; i++) {
+			int r = p + q;
+			System.out.println(r);
+			p = q;
+			q = r;
+		}
+	}
+
+	public static void main(String[] args) {
+		Fibonacci f = new Fibonacci();
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a number to check--");
+		int n = sc.nextInt();
+		f.ad(n);
+	}
+
+}
